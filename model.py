e6150dbae2505db1b18e108fa8684c41eb5c0b69
mirunalini277 <277mirunalini@gmail.com>
2025-12-30 17:26:23 +0530

    Added gitignore,requirement.txt

--- model.py
+++ model.py
@@ -1,7 +1,4 @@
-import os
-import sys
 import pandas as pd 
-import numpy as np 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -26,5 +23,5 @@ y_predict = model.predict(x_test)
 
 print("Acuuracy", accuracy_score(y_test,y_predict))
 
-joblib.dump(model,"model.pkl")
+joblib.dump(model,".gitignore\model.pkl")
 print("Model Saved Successfully.")
